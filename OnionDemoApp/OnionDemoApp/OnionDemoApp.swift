553a066524c59e5cfe02e32dcd1ae3412f75abc8
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-18 20:56:16 +0800

    chore: Cleanup

---
+++ OnionDemoApp/OnionDemoApp/OnionDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  OnionDemoAppApp.swift
+//  OnionDemoApp
+//
+//  Created by Jun on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct OnionDemoApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
+
