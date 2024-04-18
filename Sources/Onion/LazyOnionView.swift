3888b4c036d43df141eb9738418eb433e5d1a72d
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-18 20:32:42 +0800

    feat: Added functionality to toggle layers' visibility

--- Sources/Onion/LazyOnionView.swift
+++ Sources/Onion/LazyOnionView.swift
@@ -5,4 +5,31 @@
 //  Created by Jun on 18/4/24.
 //
 
-import Foundation
+import SwiftUI
+
+public struct LazyOnionView<Layer: Identifiable & Hashable & Equatable, Content: View>: View {
+  let onion: Onion<Layer>
+  private(set) var depth: Int = 0
+  
+  @Binding
+  var hiddenLayers: Set<Layer>
+  let builder: (Onion<Layer>, _ depth: Int, _ isHidden: Bool) -> Content
+  
+  public var body: some View {
+    let isHidden = hiddenLayers.contains(onion.layer) == false
+    
+    builder(onion, depth, isHidden)
+    
+    if isHidden {
+      ForEach(onion.layers) { layer in
+        LazyOnionView(
+          onion: layer,
+          depth: depth + 1,
+          hiddenLayers: $hiddenLayers
+        ) { layer, depth, isHidden in
+          builder(layer, depth, isHidden)
+        }
+      }
+    }
+  }
+}
