181e52c1169d207414f79ed973bf191f676856c8
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-18 20:55:49 +0800

    chore: Created demo project

---
+++ OnionDemoApp/OnionDemoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnionDemoApp
+//
+//  Created by Jun on 18/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnionDemoApp/OnionDemoApp/OnionDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnionDemoAppApp.swift
+//  OnionDemoApp
+//
+//  Created by Jun on 18/4/24.
+//
+
+import SwiftUI
+
+@main
+struct OnionDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
