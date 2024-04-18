9b7ece6b188932afe1f0b3bcfdfe91d22e8a7fd4
Jun Xiu Chan <51901925+missingems@users.noreply.github.com>
2024-04-18 12:12:34 +0800

    Update Onion.swift

--- Sources/Onion/Onion.swift
+++ Sources/Onion/Onion.swift
@@ -1,2 +1,20 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+//
+//  Onion.swift
+//  Onion
+//
+//  Created by Jun on 18/4/24.
+//
+
+public final class Onion<Layer: Identifiable & Hashable & Equatable>: Identifiable {
+  public var id: Layer { self.layer }
+  public let layer: Layer
+  public var layers: [Onion<Layer>] = []
+  
+  public init(
+    layer: Layer,
+    layers: [Onion<Layer>] = []
+  ) {
+    self.layer = layer
+    self.layers = layers
+  }
+}
